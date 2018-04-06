0cc8446d68fabee27dd85218e560ceb3f8897987
Patrick Seute <patrick.seute@cox.net>
2018-04-06 12:58:47 +0000

    added remote database on mlab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require('./routes/comments'),
     
 //seedDB(); //seed the database
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://patrick:steelers@ds237389.mlab.com:37389/yelpcamp")
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");

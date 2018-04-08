04f9e8f781a1458272429a15387a7df4295f0a42
Patrick Seute <patrick.seute@cox.net>
2018-04-08 18:40:24 +0000

    update middleware to allow admins permissions

--- middleware/index.js
+++ middleware/index.js
@@ -10,7 +10,7 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
                 res.redirect("back");
             } else {
                 //does the user own this campground?
-                if(foundCampground.author.id.equals(req.user._id)){
+                if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
                     next(); 
                 } else {
                     req.flash("error", "You do not have permission to do that");
@@ -45,7 +45,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
                 req.flash("error", "Comment not found");
                 res.redirect("back");
             } else {
-                if(foundComment.author.id.equals(req.user._id)){
+                if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
                     next();
                 } else {
                     req.flash("error", "You do not have permission to do that")

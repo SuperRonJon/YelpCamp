754fcf7007af0b9d24ccc38b70a5b8b67e1bc4b9
Patrick Seute <patrick.seute@cox.net>
2018-04-08 17:32:58 +0000

    added promotion route to admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var Users = require('../models/user');
 var middleware = require('../middleware');
 
+//INDEX
 router.get('/', middleware.checkAdmin, function(req, res){
     Users.find({}, function (err, users){
         if(err){
@@ -20,6 +21,24 @@ router.get('/', middleware.checkAdmin, function(req, res){
     });
 });
 
+router.get('/promote', middleware.checkAdmin, function(req, res){
+    Users.find({}, function(err, users){
+        if(err){
+            console.log(err);
+            res.redirect('back');
+        } else {
+            var admins = [];
+            users.forEach(function(user){
+                if(user.isAdmin){
+                    admins.push(user);
+                }
+            });
+            res.render('/admin/promote', {admins: admins});
+        }
+    });
+});
+
+//ADMIN DELETION, REMOVE ADMIN STATUS FROM AN ACCOUNT
 router.delete('/:admin_id', middleware.checkAdmin, function(req, res){
     Users.findByIdAndUpdate(req.params.admin_id, { $set: { isAdmin: false }}, function(err, updatedAdmin){
         if(err){
